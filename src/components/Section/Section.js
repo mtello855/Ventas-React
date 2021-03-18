b9cb7377d457b0af1086e2058ea0a461e0831201
Mariana Tello <mtello855@gmail.com>
2021-03-18 16:06:52 -0300

    incorporation of cards to the section

--- src/components/Section/Section.js
+++ src/components/Section/Section.js
@@ -1,25 +1,45 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Section.css";
 import Casco from "../../Img/casco.jpg";
+import Aceite from "../../Img/aceite.jpg";
+import Baul from "../../Img/baul.jpg";
+import OtroCasco from "../../Img/otrocasco.jpg";
+import Rueda from "../../Img/rueda.png";
+import Vidrio from "../../Img/vidrio.jpg";
 
-const cascoCard = Casco;
+const productCards = [
+  {
+    img: Casco,
+    desc: "Antiparras superpuestas",
+  },
+  { img: Aceite, desc: "Aceite 2 tiempos" },
+  { img: Baul, desc: "Especial repartidores" },
+  { img: OtroCasco, desc: "Especial repartidores" },
+  { img: Rueda, desc: "Especial repartidores" },
+  { img: Vidrio, desc: "Especial repartidores" },
+];
 
 function Section() {
+  const [testHook, setTestHook] = useState(0);
   return (
-    <div className="card-product">
-      <h2>Product Card</h2>
-      <div className="card">
-        <img className="img-Card" src={cascoCard} />
-        <h1>Tailored Jeans</h1>
-        <p className="price">$19.99</p>
-        <p>
-          Some text about the jeans. Super slim and comfy lorem ipsum lorem
-          jeansum. Lorem jeamsun denim lorem jeansum.
-        </p>
-        <p>
-          <button>Add to Cart</button>
-        </p>
-      </div>
+    <div className="cardsGrid">
+      {productCards.map((e, i) => {
+        console.log(i);
+        return (
+          <div className="card-product">
+            <h2>Product Card</h2>
+            <div className="card">
+              <img className="img-Card" src={e.img} />
+              <h1>Tailored Jeans</h1>
+              <p className="price">$19.99</p>
+              <p className="descriptionCard">{e.desc}</p>
+              <p>
+                <button>Add to Cart</button>
+              </p>
+            </div>
+          </div>
+        );
+      })}
     </div>
   );
 }
